adbcd620dce8a9e2f155a0ddde77d538cf859af0
evolshenanigans <gjgutierrez91@gmail.com>
2023-10-30 14:58:55 -0700

    modified:   app/page.tsx 	modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,122 @@
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+        <header className="w-full text-center mb-8">
+          <h1>Privacy Policy and How to Delete Info</h1>
+        </header>
+        <section className="mb-8">
+          <h2>How to Delete Info</h2>
+          <p>
+            If you wish to delete your account and discontinue using
+            SnoozeSense, please send an email to
+            <a
+              href="mailto:dreamteamcolab2@gmail.com"
+              className="text-blue-500 underline"
+            >
+              dreamteamcolab2@gmail.com
+            </a>{" "}
+            from the email address associated with your SnoozeSense account,
+            requesting account deletion. Upon receipt of your request, we will
+            process your account deletion promptly. Please be aware that once
+            your account is deleted, all associated data will be permanently
+            removed from our servers, and this action cannot be reversed. It's
+            important to ensure that you are ready to lose access to your
+            account and all associated data before sending a deletion request.
+            Your privacy and satisfaction are our priority, and our team is here
+            to assist with the process to ensure a smooth experience.
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        </section>
+        <section>
+          <h2>Privacy Policy for SnoozeSense</h2>
+          <article>
+            <p>
+              <strong>Privacy Policy for SnoozeSense</strong>
+              <br />
+              Last updated: (insert date)
+              <br />
+              <br />
+              SnoozeSense (&quot;we&quot;, &quot;our&quot;, or &quot;us&quot;)
+              operates the SnoozeSense application (the &quot;Service&quot;).
+              This page informs you of our policies regarding the collection,
+              use, and disclosure of personal information when you use our
+              Service.
+              <br />
+              <br />
+              We will not use or share your information with anyone except as
+              described in this Privacy Policy. By using the Service, you agree
+              to the collection and use of information in accordance with this
+              policy.
+              <br />
+              <br />
+              <strong>Information Collection And Use</strong>
+              <br />
+              We collect your email address to help determine your sleep time
+              and to provide and improve our Service to you.
+              <br />
+              <br />
+              <strong>Log Data</strong>
+              <br />
+              We value your trust in providing us your Personal Information,
+              thus we are striving to use commercially acceptable means of
+              protecting it. But remember that no method of transmission over
+              the internet, or method of electronic storage is 100% secure and
+              reliable, and we cannot guarantee its absolute security.
+              <br />
+              <br />
+              <strong>Service Providers</strong>
+              <br />
+              We do not share your data with third-party companies or
+              individuals. Your data is stored securely in Firebase.
+              <br />
+              <br />
+              <strong>Security</strong>
+              <br />
+              The security of your Personal Information is important to us, and
+              your data is securely stored in Firebase. However, remember that
+              no method of transmission over the internet, or method of
+              electronic storage is 100% secure. While we strive to use
+              commercially acceptable means to protect your Personal
+              Information, we cannot guarantee its absolute security.
+              <br />
+              <br />
+              <strong>User Rights</strong>
+              <br />
+              You have the right to access, correct, or delete your personal
+              information at any time. Please contact us at{" "}
+              <a
+                href="mailto:dreamteamcolab2@gmail.com"
+                className="text-blue-500 underline"
+              >
+                dreamteamcolab2@gmail.com
+              </a>{" "}
+              for any inquiries or requests regarding your data.
+              <br />
+              <br />
+              <strong>Changes To This Privacy Policy</strong>
+              <br />
+              We may update our Privacy Policy from time to time. We will notify
+              you of any changes by sending you an email.
+              <br />
+              <br />
+              <strong>Contact Us</strong>
+              <br />
+              If you have any questions or suggestions about our Privacy Policy,
+              do not hesitate to contact us at{" "}
+              <a
+                href="mailto:dreamteamcolab2@gmail.com"
+                className="text-blue-500 underline"
+              >
+                dreamteamcolab2@gmail.com
+              </a>
+              .
+            </p>
+          </article>
+        </section>
       </div>
     </main>
-  )
+  );
 }
